e75e4d1907dc582afc3e7258c75449307d8a5592
Hansika Colomboge Dona <hansikajayawardana10@hotmail.com>
2022-10-12 16:04:41 +0200

    API Second Challenge

--- weather.py
+++ weather.py
@@ -11,11 +11,26 @@ def search_city(query):
     '''Look for a given city. If multiple options are returned, have the user choose between them.
        Return one city (or None)
     '''
-    pass  # YOUR CODE HERE
+    url = urllib.parse.urljoin(BASE_URI, '/geo/1.0/direct')
+    response = requests.get(url, params = {'q': query}).json()
+    if len(response) > 1:
+        print("These are two or more cities")
+    if len(response) == 0:
+        return None
+    if not response:
+        return None
+    return response[0]
 
 def weather_forecast(lat, lon):
+
     '''Return a 5-day weather forecast for the city, given its latitude and longitude.'''
-    pass  # YOUR CODE HERE
+    url = f"https://weather.lewagon.com/data/2.5/forecast?lat={lat}&lon={lon}"
+    response = requests.get(url).json()
+    forecast = []
+    for i in response['list']:
+        if '12:00:00' in i['dt_txt']:
+            forecast.append(i)
+    return forecast
 
 def main():
     '''Ask user for a city and display weather forecast'''
